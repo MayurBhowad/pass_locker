a48dcfdc963c04638cc89906daa9cc4dc4efdb76
MayurBhowad <mayurbhowad1995@gmail.com>
2024-06-20 18:42:24 +0530

    removed netlyfy toml

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const serverless = require("serverless-http");
-
 const routes = require("./routes");
 
 const app = express();
@@ -11,5 +9,5 @@ app.use(bodyParser.json());
 
 app.use("/", routes);
 
-// app.listen(3000, () => console.log("app is running on port 3000"));
-module.exports.handler = serverless(app);
+app.listen(3000, () => console.log("app is running on port 3000"));
+// module.exports.handler = serverless(app);
